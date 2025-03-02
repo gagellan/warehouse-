64a7b6cfb496d26c67b5e89755b1d7bc75130390
Isha Solanki <ishasolanki0225@gmail.com>
2025-03-02 17:47:24 +0530

    import

--- src/components/frontend/Login.js
+++ src/components/frontend/Login.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../assets/css/App.css";
+import axios from "axios";
 import warehouseImage from "../../assets/images/download8.jpeg";
 
 const Login = () => {
--- src/components/frontend/Register.js
+++ src/components/frontend/Register.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "../../assets/css/App.css";
 import warehouseImage from "../../assets/images/download8.jpeg";
 
